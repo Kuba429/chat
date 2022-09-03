2bcf72d77a921f1a52472b4b89878ee3b2ea99dd
Kuba429 <jakubzawadzki429@gmail.com>
2022-09-03 14:18:50 +0200

    fixed a mistake: hub was created on every new connection

--- server/pkg/server/socket.go
+++ server/pkg/server/socket.go
@@ -12,8 +12,9 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
+var hub = CreateHub()
+
 func HandleSocket(w http.ResponseWriter, r *http.Request) {
-	var hub = CreateHub()
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
