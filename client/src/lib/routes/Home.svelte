8f3d2ac382fa989c60a1f5f725cbaa1c04e80b23
Kuba429 <jakubzawadzki429@gmail.com>
2022-09-07 20:01:28 +0200

    fixed bug with casing (macos fs is case insensitive and it breaks stuff)

---
+++ client/src/lib/routes/Home.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { navigateTo } from "svelte-router-spa";
+	import { getUsername, setUsername } from "../username";
+	let roomValue: string;
+	let usernameValue = getUsername();
+	const handleClick = (e) => {
+		setUsername(usernameValue);
+		navigateTo("/r/" + roomValue);
+	};
+</script>
+
+<div>
+	<label>Nickname<input bind:value={usernameValue} type="text" /></label>
+	<label>Room<input bind:value={roomValue} type="text" /></label>
+	<button on:click={handleClick}>Go</button>
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		height: 100%;
+	}
+</style>
---
+++ client/src/lib/routes/Room.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { onDestroy, setContext } from "svelte";
+	import { Connection } from "../connection";
+	import MessagesContainer from "../components/MessagesContainer.svelte";
+	import InputPanel from "../components/InputPanel.svelte";
+	import type { message } from "../types";
+	import { writable } from "svelte/store";
+	import Header from "../components/Header.svelte";
+	export let currentRoute;
+	const roomId = currentRoute.namedParams.room;
+	let conn: Connection = new Connection(roomId);
+	onDestroy(() => {
+		conn.close();
+		conn = null;
+	});
+
+	setContext("messagesStore", writable([] as message[]));
+	setContext("conn", conn);
+</script>
+
+<div>
+	<Header />
+	<MessagesContainer />
+	<InputPanel />
+</div>
+
+<style>
+	div {
+		height: 100%;
+		background-color: var(--primary);
+		display: flex;
+		flex-direction: column;
+		/* padding: 10px; */
+	}
+</style>
