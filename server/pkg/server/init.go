c81fcf79b89203a762c35e8159d563bbf06892f1
Kuba429 <jakubzawadzki429@gmail.com>
2022-09-07 21:11:06 +0200

    load port from env if available

--- server/pkg/server/init.go
+++ server/pkg/server/init.go
@@ -1,12 +1,19 @@
 package server
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func Init() {
+	PORT := os.Getenv("PORT")
+	if len(PORT) < 1 {
+		PORT = "3000"
+	}
+	fmt.Printf("port: %s \n", PORT)
 	log.Println("server init")
 	http.HandleFunc("/ws", HandleSocket)
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	log.Fatal(http.ListenAndServe(":"+PORT, nil))
 }
