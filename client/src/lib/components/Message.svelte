845d2180f1afb726f9fe4f3fefb36d29938691ad
Kuba429 <jakubzawadzki429@gmail.com>
2022-09-07 19:09:14 +0200

    fix displayed date; mirror username tag in own messages

--- client/src/lib/components/Message.svelte
+++ client/src/lib/components/Message.svelte
@@ -4,12 +4,31 @@
 	import type { message } from "../types";
 	export let message: message;
 	const conn: Connection = getContext("conn");
-	const dateRaw = new Date(message.Date);
-	const date = `${dateRaw.getHours()}:${dateRaw.getMinutes()}`;
+	let dateRaw: Date;
+	let date: string;
+	let isOwn: boolean;
+	$: {
+		// idk i don't really understand why i need to do this stuff inside this $ but it doesn't seem to be working without it. It's like the script tag only sees the first message (html sees all messages as it's supposed to though).
+		isOwn = message.SenderId === conn.id;
+		dateRaw = new Date(message.Date);
+		const h = dateRaw.getHours().toString();
+		const m = dateRaw.getMinutes().toString();
+		// put 0s at the beginning of each single digit number
+		date = `${h.length < 2 ? "0" + h : h}:${m.length < 2 ? "0" + m : m}`;
+	}
 </script>
 
-<div class={message.SenderId === conn.id ? "own" : ""}>
-	<p class="username">{message.SenderName} <span>{date}</span></p>
+<div class={isOwn ? "own" : ""}>
+	<p class="username">
+		<!-- Reversing order here instead of in css because changing display of p messes up span position (it's not perfectly aligned with text outside of span tag) -->
+		{#if isOwn}
+			<span class="date">{date}</span>
+			{message.SenderName}
+		{:else}
+			{message.SenderName}
+			<span class="date">{date}</span>
+		{/if}
+	</p>
 	<p>{message.Data}</p>
 </div>
 
@@ -30,16 +49,15 @@
 		background-color: var(--contrasting-dimm);
 		color: var(--primary);
 	}
-
 	p {
 		padding: 0;
 		margin: 0;
 	}
-	.username {
+	p.username {
 		font-size: 1.2rem;
 		font-weight: 500;
 	}
-	.username span {
+	p.username span.date {
 		font-size: 0.6em;
 		opacity: 70%;
 	}
