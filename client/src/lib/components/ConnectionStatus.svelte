5feb58f27d7b2fca8c17a21ff224a9467ee1ab54
Kuba429 <jakubzawadzki429@gmail.com>
2022-09-14 11:03:44 +0200

    extracted connection status to separate component

---
+++ client/src/lib/components/ConnectionStatus.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { roomStatusStore } from "../stores/roomStatus";
+	import { connStatus } from "../stores/connStatus";
+	// todo: display usernames of active users on hover; maybe modal on click for mobile
+</script>
+
+{#if $connStatus}
+	<span
+		><i class="fa-sharp fa-solid fa-circle green" />
+		{$roomStatusStore.users.length}</span
+	>
+{:else}
+	<i class="fa-sharp fa-solid fa-circle red" />
+{/if}
+
+<style>
+	span {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 5px;
+	}
+	i {
+		font-size: 80%;
+	}
+	i.green {
+		color: var(--green-online);
+	}
+	i.red {
+		color: var(--red-offline);
+	}
+</style>
--- client/src/lib/components/Header.svelte
+++ client/src/lib/components/Header.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-	import { roomStatusStore } from "../stores/roomStatus";
 	import { getUsername } from "../username";
 	import UsernameModal from "./UsernameModal.svelte";
-	import { connStatus } from "../stores/connStatus";
+	import ConnectionStatus from "./ConnectionStatus.svelte";
 
 	let username = getUsername();
 	let showModal = false;
@@ -11,14 +10,7 @@
 <header>
 	<span on:click={() => (showModal = true)} class="username">{username}</span>
 	<div>
-		{#if $connStatus}
-			<span
-				><i class="fa-sharp fa-solid fa-circle green" />
-				{$roomStatusStore.users.length}</span
-			>
-		{:else}
-			<i class="fa-sharp fa-solid fa-circle red" />
-		{/if}
+		<ConnectionStatus />
 	</div>
 </header>
 {#if showModal}
@@ -40,20 +32,5 @@
 	}
 	span.username {
 		font-size: 1.5rem;
-	}
-	div > span {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		gap: 5px;
-	}
-	i {
-		font-size: 80%;
-	}
-	i.green {
-		color: var(--green-online);
-	}
-	i.red {
-		color: var(--red-offline);
 	}
 </style>
