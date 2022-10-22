5be7c57c257dc709cf201fa2ffb335b8af66640d
Kuba429 <jakubzawadzki429@gmail.com>
2022-10-22 15:21:50 +0200

    replace urls with anchor tags, key messages

--- client/src/lib/components/Message.svelte
+++ client/src/lib/components/Message.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { getContext } from "svelte";
+	import { getContext, onMount } from "svelte";
 	import type { Connection } from "../connection";
 	import type { message } from "../types";
 	export let message: message;
@@ -7,15 +7,22 @@
 	let dateRaw: Date;
 	let date: string;
 	let isOwn: boolean;
-	$: {
-		// idk i don't really understand why i need to do this stuff inside this $ but it doesn't seem to be working without it. It's like the script tag only sees the first message (html sees all messages as it's supposed to though).
-		isOwn = message.SenderId === conn.id;
-		dateRaw = new Date(message.Date);
-		const h = dateRaw.getHours().toString();
-		const m = dateRaw.getMinutes().toString();
-		// put 0s at the beginning of each single digit number
-		date = `${h.length < 2 ? "0" + h : h}:${m.length < 2 ? "0" + m : m}`;
-	}
+	let preElement;
+
+	isOwn = message.SenderId === conn.id;
+	dateRaw = new Date(message.Date);
+	const h = dateRaw.getHours().toString();
+	const m = dateRaw.getMinutes().toString();
+	// put 0s at the beginning of each single digit number
+	date = `${h.length < 2 ? "0" + h : h}:${m.length < 2 ? "0" + m : m}`;
+
+	onMount(() => {
+		// replace urls with anchor tags
+		preElement.innerHTML = preElement.innerHTML.replace(
+			/((https|http)?:\/\/[^\s]+)/g,
+			(url: string) => `<a href="${url}" target="_blank">${url}</a>`
+		);
+	});
 </script>
 
 <div class={`${isOwn ? "own" : ""} ${message?.IsPending ? "dim" : ""}`}>
@@ -29,7 +36,7 @@
 			<span class="date">{date}</span>
 		{/if}
 	</p>
-	<pre>{message.Data}</pre>
+	<pre bind:this={preElement}>{message.Data}</pre>
 	{#if message.Image}
 		<img src={message.Image} alt="image sent by {message.SenderName}" />
 	{/if}
@@ -85,6 +92,9 @@
 		margin: 0;
 		word-break: break-word;
 		white-space: pre-wrap;
+	}
+	:global(div pre > a) {
+		color: inherit;
 	}
 	p.username {
 		font-size: 1.2rem;
--- client/src/lib/components/MessagesContainer.svelte
+++ client/src/lib/components/MessagesContainer.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div>
-	{#each $messagesStore as message}
+	{#each $messagesStore as message (message.Id)}
 		<Message {message} />
 	{/each}
 	<!-- div below creates free space between header and first message when user scrolls to the top of the container. For some reason margin is not working as it's supposed to in this case. It's dumb but it works 🤷‍♂️ -->
